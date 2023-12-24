19b89369af75ba70741a77b8d8584f9ba03d24e1
RUDRA <rudra@RUDRAs-MacBook-Air.local>
2023-12-25 00:03:14 +0530

    m

--- week-1/offline-class-1/level-1/01-String.js
+++ week-1/offline-class-1/level-1/01-String.js
@@ -17,7 +17,7 @@ function findIndexOf(str, target) {
   console.log("Original String:", str);
   console.log("Index:", str.indexOf(target));
 }
-findIndexOf("Hello World", "World");
+findIndexOf("Hello World", "World"); 
 
 // lastIndexOf
 function findLastIndexOf(str, target) {
